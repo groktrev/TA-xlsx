43f8d3a79aac030b03a10ae9e2f0769caa51937f
splunk <splunk@splunk-rhel10.wtfsplunk.com>
2026-06-24 20:10:53 -0400

    Initial commit

---
+++ bin/Stream-Xlsx.ps1
@@ -0,0 +1,22 @@
+#!/bin/pwsh
+
+Add-Type -Path ([System.IO.Path]::Combine($PSScriptRoot, "netstandard.dll"))
+Add-Type -Path ([System.IO.Path]::Combine($PSScriptRoot, "Sylvan.Data.Excel.dll"))
+Add-Type -Path ([System.IO.Path]::Combine($PSScriptRoot, "Sylvan.Data.Csv.dll"))
+
+$stdin = [System.Console]::OpenStandardInput()
+$stdout = [System.Console]::Out
+
+$reader = [Sylvan.Data.Excel.ExcelDataReader]::Create($stdin, [Sylvan.Data.Excel.ExcelWorkbookType]::ExcelXml)
+$writer = [Sylvan.Data.Csv.CsvDataWriter]::Create($stdout)
+
+# We can iterate over worksheets with ExcelDataReader.NextResult() and
+# retrieve the worksheet name with ExcelDataReader.WorksheetName, but we
+# can't modify the source field midstream. Only the first worksheet's
+# header row will be recognized by INDEX_EXTRACTIONS = csv.
+
+#do {
+#    $writer.Write($reader) | Out-Null
+#} while ($reader.NextResult())
+
+$writer.Write($reader) | Out-Null
